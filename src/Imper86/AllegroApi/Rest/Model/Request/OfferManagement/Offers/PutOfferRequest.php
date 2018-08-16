02f9ce446b3af9b4392af7977831abd15abee128
imper <me@imper.info>
2018-08-16 11:37:45 +0200

    added simple PutOfferRequest model

---
+++ src/Imper86/AllegroApi/Rest/Model/Request/OfferManagement/Offers/PutOfferRequest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright: IMPER.INFO Adrian Szuszkiewicz
+ * Date: 16.08.18
+ * Time: 11:36
+ */
+
+namespace Imper86\AllegroApi\Rest\Model\Request\OfferManagement\Offers;
+
+
+use Imper86\AllegroApi\Rest\Model\RequestInterface;
+use Imper86\AllegroApi\RestClientInterface;
+
+class PutOfferRequest implements RequestInterface
+{
+    /**
+     * @var string
+     */
+    private $offerId;
+    /**
+     * @var array
+     */
+    private $requestBody;
+
+    public function __construct(string $offerId, array $requestBody)
+    {
+        $this->offerId = $offerId;
+        $this->requestBody = $requestBody;
+    }
+
+    public function getMethod(): string
+    {
+        return 'PUT';
+    }
+
+    public function getUri(): string
+    {
+        return "sale/offers/{$this->offerId}";
+    }
+
+    public function getContentType(): ?string
+    {
+        return RestClientInterface::CONTENT_TYPE_BETA;
+    }
+
+    public function getBody(): ?array
+    {
+        return $this->requestBody;
+    }
+
+    public function getQuery(): ?array
+    {
+        return null;
+    }
+
+
+}
