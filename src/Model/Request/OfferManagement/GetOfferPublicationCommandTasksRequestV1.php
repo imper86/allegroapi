978ad63427122c9c159fed8cb4050f2197332b5e
imper <me@imper.info>
2019-07-10 16:54:57 +0200

    added limit/offset query params

--- src/Model/Request/OfferManagement/GetOfferPublicationCommandTasksRequestV1.php
+++ src/Model/Request/OfferManagement/GetOfferPublicationCommandTasksRequestV1.php
@@ -11,16 +11,17 @@ namespace Imper86\AllegroRestApiSdk\Model\Request\OfferManagement;
 use GuzzleHttp\Psr7\Request;
 use Imper86\AllegroRestApiSdk\Model\Request\ContentType;
 use Imper86\AllegroRestApiSdk\Model\Request\RequestTrait;
+use function GuzzleHttp\Psr7\build_query;
 
 class GetOfferPublicationCommandTasksRequestV1 extends Request
 {
     use RequestTrait;
 
-    public function __construct($token, string $commandId)
+    public function __construct($token, string $commandId, ?int $limit = null, ?int $offset = null)
     {
         parent::__construct(
             'GET',
-            "/sale/offer-publication-commands/{$commandId}/tasks",
+            "/sale/offer-publication-commands/{$commandId}/tasks?" . build_query(['limit' => $limit, 'offset' => $offset]),
             $this->prepareHeaders($token, ContentType::PUBLIC_V1)
         );
     }
