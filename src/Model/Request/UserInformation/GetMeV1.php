5002ee67beae7178afa1610da017f1e3cf1f10dc
imper <me@imper.info>
2019-10-01 09:35:28 +0200

    added GET /me request model

---
+++ src/Model/Request/UserInformation/GetMeV1.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright: IMPER.INFO Adrian Szuszkiewicz
+ * Date: 01.10.2019
+ * Time: 09:09
+ */
+
+namespace Imper86\AllegroRestApiSdk\Model\Request\UserInformation;
+
+
+use GuzzleHttp\Psr7\Request;
+use Imper86\AllegroRestApiSdk\Constants\ContentType;
+use Imper86\AllegroRestApiSdk\Model\Request\RequestTrait;
+
+class GetMeV1 extends Request
+{
+    use RequestTrait;
+
+    public function __construct($token)
+    {
+        parent::__construct('GET', '/me', $this->prepareHeaders($token, ContentType::PUBLIC_V1));
+    }
+}
