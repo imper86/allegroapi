c9c355eb47e7d9935c98e563fb7763893f8a7fcd
imper <me@imper.info>
2021-08-04 11:57:10 +0200

    added Accept-Language header to RequestTrait.php

--- src/Model/Request/RequestTrait.php
+++ src/Model/Request/RequestTrait.php
@@ -7,7 +7,6 @@
 
 namespace Imper86\AllegroRestApiSdk\Model\Request;
 
-
 use DateTime;
 use DateTimeZone;
 use GuzzleHttp\Psr7\Uri;
@@ -17,30 +16,42 @@ use function GuzzleHttp\Psr7\build_query;
 
 trait RequestTrait
 {
-    private function prepareHeaders($token, string $contentType, ?string $accept = null): array
-    {
+    private function prepareHeaders(
+        $token,
+        string $contentType,
+        ?string $accept = null,
+        ?string $acceptLanguage = null
+    ): array {
         return [
-            'Authorization' => "Bearer {$token}",
-            'Accept' => $accept ?: $contentType,
-            'Content-Type' => $contentType,
+            "Authorization" => "Bearer {$token}",
+            "Accept" => $accept ?: $contentType,
+            "Content-Type" => $contentType,
+            "Accept-Language" => $acceptLanguage ?: "pl-PL",
         ];
     }
 
-    private function prepareUri(string $host, string $path, ?array $query = null): UriInterface
-    {
+    private function prepareUri(
+        string $host,
+        string $path,
+        ?array $query = null
+    ): UriInterface {
         $uri = new Uri("https://{$host}");
         $uri = $uri->withPath($path);
 
         return !empty($query) ? $uri->withQuery(build_query($query)) : $uri;
     }
 
-    private function prepareApiUri(string $path, ?array $query = null): UriInterface
-    {
+    private function prepareApiUri(
+        string $path,
+        ?array $query = null
+    ): UriInterface {
         return $this->prepareUri(EndpointHost::API, $path, $query);
     }
 
-    private function prepareUploadUri(string $path, ?array $query = null): UriInterface
-    {
+    private function prepareUploadUri(
+        string $path,
+        ?array $query = null
+    ): UriInterface {
         return $this->prepareUri(EndpointHost::UPLOAD, $path, $query);
     }
 
@@ -50,8 +61,8 @@ trait RequestTrait
             return null;
         }
 
-        $dateTime->setTimezone(new DateTimeZone('UTC'));
+        $dateTime->setTimezone(new DateTimeZone("UTC"));
 
-        return $dateTime->format('Y-m-d\TH:i:s.v\Z');
+        return $dateTime->format("Y-m-d\TH:i:s.v\Z");
     }
 }
