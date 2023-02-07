407f1111efce010c9991b2e415937c5bfd3c0bbb
imper <me@imper.info>
2023-02-07 16:50:46 +0100

    code style improvements

--- src/Model/Request/OrderManagement/GetParcelTrackingRequestV1.php
+++ src/Model/Request/OrderManagement/GetParcelTrackingRequestV1.php
@@ -10,13 +10,14 @@ class GetParcelTrackingRequestV1 extends Request
 {
     use RequestTrait;
 
-    public function __construct($token, string $carrier, array $waybills)
+    public function __construct($token, string $carrierId, array $waybills)
     {
         parent::__construct(
             'GET',
-            $this->prepareApiUri("/order/carriers/{$carrier}/tracking", [
-                'waybill' => $waybills,
-            ]),
+            $this->prepareApiUri(
+                "/order/carriers/$carrierId/tracking",
+                ['waybill' => $waybills]
+            ),
             $this->prepareHeaders($token, ContentType::PUBLIC_V1)
         );
     }
